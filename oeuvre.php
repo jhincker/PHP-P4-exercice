197c7cdc2c60072befff0a9ac74c3ecaa9ebb0b0
Jerome Hincker <jhincker@gmail.com>
2025-10-17 14:01:18 +0200

    Redirection erreur

--- oeuvre.php
+++ oeuvre.php
@@ -1,24 +1,37 @@
 <?php
-    include 'header.php';
-    include 'bdd.php';
+include 'header.php';
+include 'bdd.php';
 
-    $bdd = connexion();
-    
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
-        header('Location: index.php');
-    }
+$bdd = connexion();
 
+// Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
+if (empty($_GET['id'])) {
+    header('Location: index.php');
+    exit();
+}
+
+$oeuvre = null;
+$error = false;
+
+try {
     $requete = $bdd->prepare('SELECT * FROM `oeuvres` WHERE id = ?');
     $requete->execute([$_GET['id']]);
     $oeuvre = $requete->fetch();
 
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
+    // Si aucune oeuvre trouvée, on marque une erreur
+    if ($oeuvre === false) {
+        $error = true;
     }
-?>
+} catch (Exception $e) {
+    $error = true;
+}
 
+// Si erreur, redirection
+if ($error) {
+    header('Location: index.php');
+    exit();
+}
+?>
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
         <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
@@ -27,7 +40,7 @@
         <h1><?= $oeuvre['titre'] ?></h1>
         <p class="description"><?= $oeuvre['artiste'] ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+            <?= $oeuvre['description'] ?>
         </p>
     </div>
 </article>
