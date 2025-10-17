f86f97744d686b584a6c932336e8e16623a065c0
Jerome Hincker <jhincker@gmail.com>
2025-10-17 14:22:46 +0200

    Ajout Required Input Form

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <?php
-    include 'header.php';
-    include 'bdd.php';
+include 'header.php';
+include 'bdd.php';
 
+try {
     $bdd = connexion();
     $oeuvres = $bdd->query('SELECT * FROM oeuvres');
+    $error = false;
+    // Si aucune oeuvre trouvée, on marque une erreur
+    if ($oeuvres === false) {
+        $error = true;
+    }
+} catch (Exception $e) {
+    $error = true;
+}
+
+// Si erreur, redirection
+if ($error) {
+    die('Erreur lors de la récupération des oeuvres en base de données.');
+}
 ?>
 <div id="liste-oeuvres">
-    <?php foreach($oeuvres as $oeuvre): ?>
+    <?php foreach ($oeuvres as $oeuvre): ?>
         <article class="oeuvre">
             <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
                 <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
